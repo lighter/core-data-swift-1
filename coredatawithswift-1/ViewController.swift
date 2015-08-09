e9895d28feae8a181af5edc0bc53b068466e780e
lighter <apple.xva@gmail.com>
2015-08-09 22:17:24 +0800

    Initial Commit

---
+++ coredatawithswift-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coredatawithswift-1
+//
+//  Created by lighter on 2015/8/9.
+//  Copyright (c) 2015年 Lighter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
